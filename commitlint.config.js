a20b74046a617e85e7dd4243532c386f54d89dbf
hienvukhmt93 <hienvukhmt93@gmail.com>
2020-12-25 21:08:09 +0700

    update(notification): change commit

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['update', 'fix', 'change', 'hotfix']],
+        'header-max-length': [2, 'always', 99],
+        'footer-leading-blank': [2, 'always', 99],
+        'scope-case': [2, 'always', 'lower-case'],
+    },
+};
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+const a = 10;
+
+const calculator = (): number => a;
+
+export {calculator};
