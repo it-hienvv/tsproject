9749420c353d25880b89cac5f57cdf4f885b0381
hienvukhmt93 <hienvukhmt93@gmail.com>
2020-12-28 14:59:30 +0700

    update(base): change base

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,6 +1,7 @@
+import {SagaIterator} from 'redux-saga';
 import {all, fork} from 'redux-saga/effects';
 import * as toDoListSagas from './toDo';
 
-export default function* rootSaga() {
+export default function* rootSaga(): SagaIterator<void> {
     yield all([...Object.values(toDoListSagas)].map(fork));
 }
--- src/sagas/toDo.ts
+++ src/sagas/toDo.ts
@@ -1,10 +1,11 @@
+import {SagaIterator} from 'redux-saga';
 import {all, takeLatest} from 'redux-saga/effects';
 
-function* requestCheckToDoList(action) {
+function* requestCheckToDoList(action: string): SagaIterator<void> {
     console.log('action', action);
 }
 
-function* fetchWatcher() {
+function* fetchWatcher(): SagaIterator<void> {
     yield all([takeLatest('ACTION_ANY', requestCheckToDoList)]);
 }
 
